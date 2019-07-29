3d7c735c1683c2e82a2a3c8a0646d3b3286594e9
Vincent Honca <vincenthonca@iMac-de-Vincent.lan>
2019-07-29 14:32:25 +0200

    Making Two Methods,one for creating FileTypeFolder in given unsorted folder with the getFileType method

--- src/intecbrussel/be/SortingSoftware.java
+++ src/intecbrussel/be/SortingSoftware.java
@@ -1,8 +1,26 @@
 package intecbrussel.be;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class SortingSoftware {
 
     public static void main(String[] args) {
+
+
+        //Path from the unsorted folder
+        String folder = "/Users/vincenthonca/Desktop/dossier";
+
+        Path path = Paths.get(folder);
+
+
+        if (path.toFile().exists()) {
+
+            for (String s : path.toFile().list((dir, name) -> !name.equals(".DS_Store"))){
+                creatingfileTypeFolder(path.toString(),getFileType(s));
+
+            }
+        }
 
 
     }
